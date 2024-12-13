9654c7b2cb1b674783f26940e86fd31ffdd8e725
jverdu-r <jorge.verdu.ruiz@gmail.com>
2024-12-13 15:15:54 +0100

    added stack container in ex01

--- ex01/src/RPN.cpp
+++ ex01/src/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: jverdu-r <jverdu-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 16:59:24 by jverdu-r          #+#    #+#             */
-/*   Updated: 2024/11/21 17:08:38 by jverdu-r         ###   ########.fr       */
+/*   Updated: 2024/12/13 15:05:00 by jverdu-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,71 +14,54 @@
 
 
 
-
 int evaluateRPN(const std::string& expression, bool& error)
 {
-    int stack[MAX_STACK_SIZE];
-    int top = -1;
+    std::stack<int> stack;
     std::istringstream tokens(expression);
     std::string token;
 
     error = false;
 
     while (tokens >> token) {
-        if (token.length() == 1 && std::isdigit(token[0]))
-		{
-            if (top + 1 >= MAX_STACK_SIZE)
-			{
-                error = true;
-                return 0;
-            }
-            stack[++top] = std::atoi(token.c_str());
-        }
-        else if (token == "+" || token == "-" || token == "*" || token == "/")
-		{
-            if (top < 1)
-			{
+        if (token.length() == 1 && std::isdigit(token[0])) {
+            stack.push(std::atoi(token.c_str()));
+        } else if (token == "+" || token == "-" || token == "*" || token == "/") {
+            if (stack.size() < 2) {
                 error = true;
                 return 0;
             }
 
-            int b = stack[top--];
-            int a = stack[top--];
+            int b = stack.top();
+            stack.pop();
+            int a = stack.top();
+            stack.pop();
             int result = 0;
 
-            if (token == "+")
-			{
+            if (token == "+") {
                 result = a + b;
-            } else if (token == "-")
-			{
+            } else if (token == "-") {
                 result = a - b;
-            } else if (token == "*")
-			{
+            } else if (token == "*") {
                 result = a * b;
-            } else if (token == "/")
-			{
-                if (b == 0)
-				{
+            } else if (token == "/") {
+                if (b == 0) {
                     error = true;
                     return 0;
                 }
                 result = a / b;
             }
 
-            stack[++top] = result;
-        }
-		else
-		{
+            stack.push(result);
+        } else {
             error = true;
             return 0;
         }
     }
 
-    if (top != 0)
-	{
+    if (stack.size() != 1) {
         error = true;
         return 0;
     }
 
-    return stack[top];
+    return stack.top();
 }
